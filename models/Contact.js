b219c4c86c10c43542006c48a00558ef30e42b28
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-06-03 15:47:24 +0530

    Delete models directory

--- models/Contact.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-const ContactSchema=new mongoose.Schema({
-    name:String,
-    Email:String,
-    msg:String
-});
-module.exports=mongoose.model("Contact",ContactSchema);
