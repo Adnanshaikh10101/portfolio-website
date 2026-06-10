c2021e2842709ebc49ed007433c705db519bb713
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-06-10 16:09:20 +0530

    Completed

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,10 +1,73 @@
-import axios from "axios";
+import "../index.css";
+import React, { useState } from "react";
+import api from "../service/api"; // ✅ add this
 
-const api = axios.create({
-  baseURL: "https://portfolio-website-backend-mszv.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+function Contact() {
+    const [form, setform] = useState({
+        name: "",
+        email: "",
+        msg: ""
+    });
 
-export default api;
+    const handleChange = (e) => {
+        setform({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!form.name || !form.email || !form.msg) {
+            alert("All Fields Required");
+            return;
+        }
+
+        try {
+            const res = await api.post("/contact", form); // ✅ axios used here
+            alert(res.data.msg);
+
+            setform({
+                name: "",
+                email: "",
+                msg: ""
+            });
+
+        } catch (err) {
+            console.log(err);
+            alert("Error Sending Message");
+        }
+    };
+
+    return (
+        <div className="Contact">
+            <h1>Contact Me</h1>
+            <form className="contact-container" onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    name="name"
+                    value={form.name}
+                    onChange={handleChange}
+                    placeholder="Enter Your Name"
+                /><br/>
+
+                <input 
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    placeholder="Enter Your Email"
+                /><br/>
+
+                <textarea   
+                    name="msg"
+                    value={form.msg}
+                    onChange={handleChange}
+                    placeholder="Enter Your Message"
+                /><br/>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Contact;
